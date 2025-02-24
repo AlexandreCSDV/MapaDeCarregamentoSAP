7443c9e5b3ce960bceeac696262e5c13760f55b9
AlexandreCSDV <alexandre.sousa@dellavolpe.com.br>
2025-02-24 14:55:20 -0300

    Update MapaDeCarregamento.py

--- MapaDeCarregamento.py
+++ MapaDeCarregamento.py
@@ -160,6 +160,7 @@ if uploaded_file is not None:
     df['ONU'] = df['ONU'].apply(lambda x: x[:-2] if x.endswith('.0') else x).replace('nan', '')
     # df['Endereco WMS'] = df['Endereco WMS'].fillna('')
     df['Peso'] = df['Peso'].replace({'.': '', ',': '.'}, regex=True)
+    df['Peso'] = df['Peso'].replace('', pd.NA)
     df['Peso'] = df['Peso'].fillna(0)
     df['Peso'] = df['Peso'].astype(float).astype(int)
     df['Peso'] = df['Peso'].round(2)
